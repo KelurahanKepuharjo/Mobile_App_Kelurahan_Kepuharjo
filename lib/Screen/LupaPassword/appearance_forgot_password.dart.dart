d79b5d63f616dc778d0a8eb0d7a0ff2cae3dea99
fwd <fwdachmd@gmail.com>
2022-11-17 21:07:03 +0700

    add forgot password

---
+++ lib/Screen/LupaPassword/appearance_forgot_password.dart.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:kepuharjo_app/Comm/getTextField.dart';
+
+import 'package:kepuharjo_app/Screen/Login/appearance_login.dart';
+import 'package:kepuharjo_app/Screen/LupaPassword/create_new_password.dart.dart';
+import 'package:kepuharjo_app/Screen/LupaPassword/verifikasi_pin.dart';
+import 'package:kepuharjo_app/Shared/shared.dart';
+import 'package:pinput/pinput.dart';
+
+class AppearanceForgotPassword extends StatefulWidget {
+  AppearanceForgotPassword({key});
+
+  @override
+  State<AppearanceForgotPassword> createState() =>
+      _AppearanceForgotPasswordState();
+}
+
+class _AppearanceForgotPasswordState extends State<AppearanceForgotPassword> {
+  final rphpController = TextEditingController();
+  final pinController = TextEditingController();
+  final focusNode = FocusNode();
+
+  @override
+  void dispose() {
+    pinController.dispose();
+    focusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const focusedBorderColor = Color.fromRGBO(23, 171, 144, 1);
+    const fillColor = Color.fromRGBO(243, 246, 249, 0);
+    const borderColor = Color.fromRGBO(23, 171, 144, 0.4);
+
+    final defaultPinTheme = PinTheme(
+      width: 56,
+      height: 56,
+      textStyle: const TextStyle(
+        fontSize: 22,
+        color: Color.fromRGBO(30, 60, 87, 1),
+      ),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(19),
+        border: Border.all(color: borderColor),
+      ),
+    );
+
+    return Scaffold(
+      backgroundColor: whiteColor,
+      appBar: AppBar(
+        leading: Padding(
+          padding: EdgeInsets.all(8),
+          child: GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const AppeareaceLogin()));
+            },
+            child: Icon(Icons.keyboard_arrow_left),
+          ),
+        ),
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.transparent,
+        shadowColor: Colors.transparent,
+        title: Text(
+          'Forgot Password',
+          style: GoogleFonts.poppins(
+              color: whiteColor, fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        flexibleSpace: Container(
+            decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20)),
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    lightGreen,
+                    midGreen,
+                  ],
+                ))),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.fromLTRB(20, 50, 20, 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Center(
+                child: Image.asset(
+                  "images/lock.png",
+                  height: 100,
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Center(
+                child: Text(
+                  "Silahkan Masukan No.Handphone Anda",
+                  style: GoogleFonts.poppins(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: blackColor,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 2,
+              ),
+              Center(
+                child: Text(
+                  "Untuk Mendapatkan Kode Verifikasi",
+                  style: GoogleFonts.poppins(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: blackColor,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 70,
+              ),
+              getTextField(
+                controller: rphpController,
+                hintName: " No.Telepon",
+                keyboardType: TextInputType.number,
+                inputFormatters: FilteringTextInputFormatter.digitsOnly,
+              ),
+              const SizedBox(
+                height: 200,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 45,
+                    width: 250,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFF2BAE82),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            )),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      const VerifikasiKode()));
+                        },
+                        child: Text(
+                          'Kirim',
+                          style: GoogleFonts.poppins(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white),
+                        )),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
