d4b1e34fd29359af92f469fff1189ac2bc173e2e
fwd <fwdachmd@gmail.com>
2022-11-28 07:58:54 +0700

    read data berita

---
+++ lib/Api_connect/Api_connect.dart
@@ -0,0 +1,11 @@
+class ApiConnect {
+  static const hostConnect = "http://192.168.0.117/Web_Kelurahan_kepuharjo";
+  static const connectApi = "$hostConnect/Api";
+
+  //login
+  static const signin = "$connectApi/signin.php";
+  //register
+  static const signup = "$connectApi/signup.php";
+  //read data berita
+  static const berita = "$connectApi/read_berita.php";
+}
