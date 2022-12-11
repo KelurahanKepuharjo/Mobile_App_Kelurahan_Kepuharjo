3c70f796a43313499459b70f1a3cd783f88d0ac3
Achmad Fawaid <fwdachmd@gmail.com>
2022-12-11 17:17:55 +0700

    test

--- lib/Shared/theme_shared.dart
+++ lib/Shared/theme_shared.dart
@@ -4,8 +4,8 @@ Color blueColor = Color(0xFF0FA8EA);
 Color whiteColor = Colors.white;
 Color blackColor = Colors.black;
 Color darkgreenColor = Colors.indigo.shade800;
-Color greyColor = Color.fromRGBO(224, 224, 224, 1);
-Color midGreen = Color(0xFF007ADF);
+Color greyColor = const Color.fromRGBO(224, 224, 224, 1);
+Color midGreen = const Color(0xFF007ADF);
 Color lightGreen = const Color(0xFF0acffe);
 
 TextStyle appTextStyle = GoogleFonts.inter(
