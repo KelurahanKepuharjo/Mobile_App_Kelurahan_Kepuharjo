7a09abfac0301f340aada7d6e075c5f6d08fd0b7
akyunkurrota4@gmail.com <100007975+KurrotaAkyun@users.noreply.github.com>
2022-12-25 00:13:05 +0700

    download surat pdf

---
+++ lib/Screen/Status/Pdf/Pdf_Pindah.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
+import 'package:path/path.dart';
+
+import 'package:kepuharjo_app/Shared/shared.dart';
+
+class PdfPindah extends StatefulWidget {
+  String url;
+  File file;
+
+  PdfPindah({Key key, this.url, this.file}) : super(key: key);
+
+  @override
+  State<PdfPindah> createState() => _PdfPindahState();
+}
+
+class _PdfPindahState extends State<PdfPindah> {
+  @override
+  Widget build(BuildContext context) {
+    final name = basename(widget.file.path);
+    return Scaffold(
+        backgroundColor: whiteColor,
+        appBar: AppBar(
+          shadowColor: whiteColor,
+          leading: GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: const Icon(Icons.keyboard_arrow_left),
+          ),
+          automaticallyImplyLeading: false,
+          backgroundColor: appColor,
+          title: Text(name),
+        ),
+        body: PDFView(
+          filePath: widget.file.path,
+        ));
+  }
+}
